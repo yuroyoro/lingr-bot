600daddfa08d33d0f5c8b5d9d9a0ccd4ac7dadca
Tomohito Ozaki <ozaki@yuroyoro.com>
2012-02-15 22:26:36 +0900

    %を含む文字列でURLDecodeが例外吐いてたので握り潰してやったぜ(ｷﾘｯ
    　 ∧,,∧
    　(；`・ω・）　　｡･ﾟ･⌒） チャーハン作るよ！！
    　/　　 ｏ━ヽニニフ))
    　しー-Ｊ
    
    　　　　　　　　アッ！　｡･ﾟ･
    　 ∧,,∧ て　　　　　｡･ﾟ･｡･ﾟ･
    　(； ´ﾟωﾟ）て　　　／／
    　/　　 ｏ━ヽニニフ
    　しー-Ｊ　　 　彡
    
    　　　　∧,,∧　　　　ｼｮﾎﾞｰﾝ
    　　　（ ´・ω・）
    　　c(,＿Ｕ_Ｕ　　　　　　･ﾟ･｡･ ﾟ･｡･ﾟ･ ｡･ﾟ･
    　　　　　━ヽニニフ
    
    　　　　　　　　　　　　よしバレてない
    　　　　　　 ｸﾙｯ　∧,,∧　　
    　　　　　　　 　ﾐ（・ω・´ ）つ　ｻｯｻ
    　　 　　　　　　c( Ｕ･ ﾟＵ｡彡･ ｡･ﾟ･　
    　━ヽニニフ
    
    　∧,,∧　　　｡･｡ﾟ･｡ ﾟ･｡ﾟ･　できたよ〜
    （ ´・ω・）つ＼･ﾟ･ ｡･ﾟ･･／

--- src/main/scala/com/yuroyoro/lingr/LingrBotFilter.scala
+++ src/main/scala/com/yuroyoro/lingr/LingrBotFilter.scala
@@ -8,6 +8,7 @@ import javax.servlet.{ServletRequest, ServletResponse}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import scala.io.{Source, Codec}
 import scala.util.parsing.json.JSON
+import scala.util.control.Exception._
 
 class LingrBotFilter extends Filter {
   override def destroy = Unit
@@ -21,17 +22,17 @@ class LingrBotFilter extends Filter {
 
     implicit val codec:Codec = Codec.UTF8
     val body = Source.fromInputStream(req.getInputStream).mkString
-    println("=" * 80)
-    println(body)
-    println("=" * 80)
+    // println("=" * 80)
+    // println(body)
+    // println("=" * 80)
     res.setContentType("text/plain")
     res.setCharacterEncoding("UTF-8")
 
     // val testdata = URLEncoder.encode("""{"status":"ok", "counter":2320356, "events":[{"message":{"id":"1216064", "room":"yuroyoro_test", "public_session_id":"KpQ9lM", "icon_url":"http://www.gravatar.com/avatar/bd3590aaffe8948079d27795cb6f7388.jpg", "type":"user", "speaker_id":"yuroyoro", "nickname":"ゆろよろ", "text":"&bijin fukuoka 0138", "timestamp":"2010-12-07T10:35:58Z", "local_id":"pending-KpQ9lM-13"}, "event_id":2320356}]}""", "utf-8")
 
     // body.split("json=").lastOption.filter{ _.trim.nonEmpty }.orElse(Option(req.getParameter("json"))).orElse(Some(testdata)).map{ s =>
-    Option(body).filter{ _.trim.nonEmpty }.map{ s =>
-      URLDecoder.decode(s, "utf-8")
+    Option(body).filter{ _.trim.nonEmpty }.flatMap{ s =>
+      allCatch.opt{ URLDecoder.decode(s, "utf-8") }
     }.foreach{ s =>
       JSON.parse(s).foreach{ json => json.collect {
         case ("events", xs) => {
